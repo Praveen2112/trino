2c7c3f0cd1ca3b722401c58dc5ebb9c801199061
praveenkrishna.d <praveenkrishna@tutanota.com>
2024-12-24 17:45:53 +0530

    Increase size to 8192

--- core/trino-main/src/main/java/io/trino/operator/HashAggregationOperator.java
+++ core/trino-main/src/main/java/io/trino/operator/HashAggregationOperator.java
@@ -346,7 +346,7 @@ public class HashAggregationOperator
         this.typeOperators = requireNonNull(typeOperators, "typeOperators is null");
 
         this.memoryContext = operatorContext.localUserMemoryContext();
-        this.hyperLogLog = HyperLogLog.newInstance(128);
+        this.hyperLogLog = HyperLogLog.newInstance(8192);
         hyperLogLog.makeDense();
     }
 
